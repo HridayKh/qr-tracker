546d5d3d3943b1c6c4c9f26efc8168f8092dab1c
HridayDev <hridaykh1234@gmail.com>
2025-05-12 13:49:07 +0530

    a

--- qr-tracker/src/main/java/servlets/Main.java
+++ qr-tracker/src/main/java/servlets/Main.java
@@ -1,6 +1,9 @@
 package servlets;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,23 +11,35 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/whatsapp")
+@WebServlet("/test")
 public class Main extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws IOException, ServletException
-    {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Hello World!</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Hello World!</h1>");
-        out.println("</body>");
-        out.println("</html>");
-    }
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello World!</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello World!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+
+		String url = "jdbc:mysql://db.hriday.tech:3306/QR_DB";
+		String user = System.getenv("REMOTE_TOMCAT_QR_DB_USER");
+		out.println(user);
+		String password = System.getenv("REMOTE_TOMCAT_QR_DB_PASSWORD");
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(url, user, password);
+			conn.close();
+			System.out.println("Connected to the database!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
